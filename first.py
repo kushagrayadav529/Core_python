11d9aff2485dfe8b4128354e3d4dfa5d6f32ef0e
kushagrayadav529 <142107940+kushagrayadav529@users.noreply.github.com>
2026-07-10 11:51:36 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,25 @@
+print("hello world");
+x=123;
+print(x)
+
+print("------------------------------------------------------")
+
+
+print(10+2)
+print(10-2)
+print(10*2)
+print(11/2)
+print(11//2)
+print(10**2)
+
+
+
+print("------------------------------------------------------")
+
+a=10
+b=5
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
